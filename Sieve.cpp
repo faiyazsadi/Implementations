58b42cdedc4890eb22266de04b231331a4ccc711
Sadi <faiyazsadi3334@gmail.com>
2021-10-20 21:15:28 +0600

    Optimized WHO

--- Sieve.cpp
+++ Sieve.cpp
@@ -3,24 +3,31 @@
 using namespace std;
 
 constexpr int N = (int) 1e6 + 10;
-vector<int> prime;
-bool is_composite[N];
+vector<int> primes;
+bool is_prime[N + 1];
 
 void sieve() {
-    fill (is_composite, is_composite + N, false);
-    for (int i = 2; i < N; ++i) {
-        if (!is_composite[i]) prime.push_back (i);
-        for (int j = 0; j < (int) prime.size () and i * prime[j] < N; ++j) {
-            is_composite[i * prime[j]] = true;
-            if (i % prime[j] == 0) break;
+    fill(is_prime, is_prime + N, true);
+    for(int i = 3; i * i <= N; i += 2) {
+        if(is_prime[i]) {
+            for(int j = i * i; j <= N; j += i * 2) {
+                is_prime[j] = false;
+            }
+        }
+    }
+    if(N > 1) primes.push_back(2);
+    for(int i = 3; i <= N; i += 2) {
+        if(is_prime[i]) {
+            primes.push_back(i);
         }
     }
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
     sieve();
+    for(int prime : primes) {
+        cout << prime << ' ';
+    }
+    cout << '\n';
     return 0;
 }
-
