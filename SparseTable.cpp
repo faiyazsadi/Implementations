a6ffc6c4033b73880c03eee4d7fd2d9a4b112ab1
Sadi <faiyazsadi3334@gmail.com>
2022-02-23 02:32:05 +0600

    sparse table in a class

--- SparseTable.cpp
+++ SparseTable.cpp
@@ -2,26 +2,31 @@
 
 using namespace std;
 
-const int N = (int) 5e5+10;
-const int LOG = 25;
-vector<vector<int>> sp(N, vector<int>(LOG));
-
-void init(int n) {
-    for(int j = 1; j < LOG; ++j) {
-        for(int i = 0; i + (1 << j) - 1 < n; ++i) {
-            sp[i][j] = min(sp[i][j - 1], sp[i + (1 << (j - 1))][j - 1]);
+class SparseTable {
+    const int N = (int) 5e5+10;
+    const int LOG = 25;
+    vector<vector<int>> sp;
+public:
+    void init(vector<int>& a, int n) {
+        sp.resize(N, vector<int>(LOG));
+        for(int i = 0; i < n; ++i) {
+            sp[i][0] = a[i];
+        }
+        for(int j = 1; j < LOG; ++j) {
+            for(int i = 0; i + (1 << j) - 1 < n; ++i) {
+                sp[i][j] = min(sp[i][j - 1], sp[i + (1 << (j - 1))][j - 1]);
+            }
         }
     }
-}
-
-int query() {
-    int l, r;
-    cin >> l >> r;
-    r--;
-    int len = r - l + 1;
-    int k = 31 - __builtin_clz(len);
-    return min(sp[l][k], sp[r - (1 << k) + 1][k]);
-}
+    int query() {
+        int l, r;
+        cin >> l >> r;
+        r--;
+        int len = r - l + 1;
+        int k = 31 - countl_zero(1u * len);
+        return min(sp[l][k], sp[r - (1 << k) + 1][k]);
+    }
+} st;
 
 int main() {
     ios::sync_with_stdio(false);
@@ -32,11 +37,10 @@ int main() {
     vector<int> a(n);
     for(int i = 0; i < n; ++i) {
         cin >> a[i];
-        sp[i][0] = a[i];
     }
-    init(n);
+    st.init(a, n);
     while(q--) {
-        int ans = query();
+        int ans = st.query();
         cout << ans << '\n';
     }
     return 0;
